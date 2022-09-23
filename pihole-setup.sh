4dca79e92187f490c13975573e7520599e463505
Robert Helgeson <son_of_helga@icloud.com~>
2022-09-23 13:42:30 -0700

    initial run
    pre trial run

--- pihole-setup.sh
+++ pihole-setup.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+sudo apt install curl -y
+sudo curl -sSL https://install.pi-hole.net | bash
+
+clear
+echo 'what would you like your pihole password to be?'
+read password
+
+pihole -a -p $password
+
+sudo apt update -y && sudo apt full-upgrade -y
+sudo apt install unbound -y
+sudo mv pi-hole.conf /etc/unbound/unbound.conf.d/pi-hole.conf
+
+sudo service unbound restart
